df46faafdf3e8c0588464a5bbd2bd5a3644b93c0
colab_nakajima <kochi.nakajima@colab.co.jp>
2020-07-27 12:07:10 +0900

    linking setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
+THIS_DIR=$PWD
 sudo apt install tmux
-mkdir -p ~/.vim/undo
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+mkdir -p $THIS_DIR/vim/undo
+cd ~/vimtest
+ln -s $THIS_DIR/.vimrc .
+ln -s $THIS_DIR/.tmux.conf .
+ln -s $THIS_DIR/.vim .
