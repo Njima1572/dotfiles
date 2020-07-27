9e1ef9f8f4007e85fce4cc8256b13b527a53fdf1
colab_nakajima <kochi.nakajima@colab.co.jp>
2020-07-27 12:07:56 +0900

    directory

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 mkdir -p $THIS_DIR/vim/undo
-cd ~/vimtest
+cd ~/
 ln -s $THIS_DIR/.vimrc .
 ln -s $THIS_DIR/.tmux.conf .
 ln -s $THIS_DIR/.vim .
