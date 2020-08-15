72a5ff4ebaa84e2b1eb45c3b25f9775af1592fa5
kochi <kochi.nakajima@gmail.com>
2020-08-15 22:15:10 +0900

    using namespace std

--- .vim/cpp/competition.cpp
+++ .vim/cpp/competition.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
 
+using namespace std;
+
 int main()
 {
   int a, b, c;
-  std::in >> a >> b >> c;
-  std::cout << a << b << c;
+  cin >> a >> b >> c;
+  cout << a << b << c;
   return 0;
 }
