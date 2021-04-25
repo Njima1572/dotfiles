76772536e9a52c696cbe26a56b5fbf11b2695368
Njima1572 <kochi.nakajima@gmail.com>
2021-04-25 11:19:17 +0900

    Added alacritty coloscheme

--- config.fish
+++ config.fish
@@ -14,5 +14,14 @@ eval (gh completion -s fish| source)
 source ~/.config/fish/.aliases.fish
 source ~/.config/fish/local_alias.fish
 
+set -x ANDROID_SDK_ROOT $HOME/Android/Sdk
+set -x ANDROID_HOME $HOME/Android/Sdk
+set -x JAVA_HOME $HOME/Library/android-studio/jre
+
 set -x PATH /home/linuxbrew/.linuxbrew/bin $PATH
+set -x PATH $JAVA_HOME/bin $PATH
+set -x PATH $ANDROID_SDK_ROOT/emulator $PATH
+set -x PATH $ANDROID_SDK_ROOT/tools $PATH
+set -x PATH $ANDROID_SDK_ROOT/tools/bin $PATH
+set -x PATH $ANDROID_SDK_ROOT/platform-tools $PATH
 set -x EDITOR nvim
