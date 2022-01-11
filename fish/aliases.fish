ebc7403ad58bbadfedf980b43718265c39a440a3
Njima1572 <kochi.nakajima@gmail.com>
2022-01-11 13:09:56 -0500

    Removed unused aliases

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -5,28 +5,3 @@ alias v="nvim"
 alias nv="nvim"
 alias mv="mv -i"
 alias ..="cd .."
-alias scd='cd ~/Singularity'
-alias mux=tmuxinator
-
-
-function copy
-  cat $argv | xsel -bi
-end
-
-function google-search
-  if set -q BROWSER
-    echo Using $BROWSER
-  else
-    set BROWSER firefox
-  end
-  $BROWSER "https://www.google.com/search?q=$argv"
-end
-  
-
-function cl
-  cd $argv | ls
-end
-
-function activate
-  source $VIRTUALENV_DIR/$argv/bin/activate.fish
-end
