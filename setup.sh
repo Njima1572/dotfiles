fb8d6fdc74e991b148cd799582c73da94387c4cd
Kochi Nakajima <kochi.nakajima@gmail.com>
2024-09-26 09:52:51 -0400

    Fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+NVIM_RELPATH=".config/nvim"
+
 install_neovim() { #{{{
   echo "Checking if neovim is already installed..."
   if [ -x "$(command -v nvim)" ]; then
@@ -37,19 +39,47 @@ install_neovim() { #{{{
 
 setup_neovim() { # {{{
   ## Setup nvim soft link
+  # Check if the symbolic link exists and is pointing to the correct directory
+  if [ -L ~/$NVIM_RELPATH ]; then
+      if [ "$(readlink ~/$NVIM_RELPATH)" == "$PWD/$NVIM_RELPATH" ]; then
+          echo "nvim symbolic link already exists"
+          return
+      else
+          echo "nvim symbolic link exists but pointing to wrong directory"
+          read -p "Do you want to remove it? [y/n] " -n 1
+          echo
+          if [[ $REPLY =~ ^[Yy]$ ]]; then
+              rm ~/$NVIM_RELPATH
+          else
+              echo "Please remove ~/$NVIM_RELPATH first"
+              exit 1
+          fi
+      fi
+  fi
   # If there is existing file, check user if they want to move it to .bk
-  if [ -d ~/.config/nvim ]; then
+  if [ -d ~/$NVIM_RELPATH]; then
       echo "nvim directory already exists"
-      read -p "Do you want to move it to ~/.config/nvim.bk? [y/n] " -n 1 -r
+      read -p "Do you want to move it to ~/$NVIM_RELPATH.bk? [y/n] " -n 1
       echo
       if [[ $REPLY =~ ^[Yy]$ ]]; then
-          mv ~/.config/nvim ~/.config/nvim.bk
+          mv ~/$NVIM_RELPATH ~/$NVIM_RELPATH.bk
       else
-          echo "Please remove ~/.config/nvim first"
+          echo "Please remove ~/$NVIM_RELPATH first"
+          exit 1
+      fi
+  # If symbolic link exists
+  elif [ -L ~/$NVIM_RELPATH ]; then
+      echo "nvim symbolic link already exists"
+      read -p "Do you want to remove it? [y/n] " -n 1
+      echo
+      if [[ $REPLY =~ ^[Yy]$ ]]; then
+          rm ~/$NVIM_RELPATH
+      else
+          echo "Please remove ~/$NVIM_RELPATH first"
           exit 1
       fi
   fi
-  ln -s ~/.dotfiles/nvim ~/.config/nvim
+  ln -s $PWD/$NVIM_RELPATH ~/$NVIM_RELPATH
 }
 # }}}
 
@@ -74,7 +104,7 @@ setup_gitconfig() { # {{{
 # Ask user if they want to install neovim dotfiles
 
 echo "This script will setup dotfiles for neovim"
-read -p "Do you want to setup neovim dotfiles? [y/n] " -n 1 -r
+read -p "Do you want to setup neovim dotfiles? [y/n] "
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo
