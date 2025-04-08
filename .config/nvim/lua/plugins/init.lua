1a796f1c023a73ba1059e642911ecc6b6e70de8d
Kochi Nakajima <kochi.nakajima@gmail.com>
2025-04-07 20:43:52 -0400

    Updates

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -111,12 +111,12 @@ return {
         }
       )
       local mason_tool_installer = require("mason-tool-installer")
-      mason_tool_installer.setup({
-        ensure_installed = {
-          'prettier',
-          'black',
-        }
-      })
+      -- mason_tool_installer.setup({
+      --   ensure_installed = {
+      --     'prettier',
+      --     'black',
+      --   }
+      -- })
     end
   },
   {
@@ -722,5 +722,34 @@ return {
       'RainbowDelimQuoted',
       'RainbowMultiDelim'
     }
+  },
+  {
+    "ravitemer/mcphub.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- Required for Job and HTTP requests
+    },
+    -- comment the following line to ensure hub will be ready at the earliest
+    cmd = "MCPHub",                          -- lazy load by default
+    build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
+    -- uncomment this if you don't want mcp-hub to be available globally or can't use -g
+    -- build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
+    config = function()
+      require("mcphub").setup({
+        auto_approve = false,
+        extensions = {
+          codecompanion = {
+          }
+        },
+        log = {
+          level = vim.log.levels.WARN,
+          to_file = false,
+          file_path = nil,
+          prefix = "MCPHub"
+        }
+      })
+    end,
+  },
+  {
+    "olimorris/codecompanion.nvim"
   }
 }
